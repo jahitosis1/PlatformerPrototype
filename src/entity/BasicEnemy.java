2df8f538cee92d8ac8ba70746b9cd36704480c64
jahitosis1 <jahitosis1@up.edu.ph>
2023-12-17 14:34:00 +0800

    feat: enemy movement, gameover, stageclear, stagemenu

---
+++ src/entity/BasicEnemy.java
@@ -0,0 +1,49 @@
+package entity;
+
+import javafx.animation.Animation;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+
+public class BasicEnemy extends Enemy {
+    public final static Image ENEMY_SPRITE_IDLE = new Image("images/Bringer-of-Death_Idle_0.png", ENEMY_SIZE, ENEMY_SIZE, false, false);
+    public final static Image ENEMY_SPRITE_MOVE7 = new Image("images/Bringer-of-Death_Walk_0.png", ENEMY_SIZE, ENEMY_SIZE, false, false);
+    public final static Image ENEMY_SPRITE_ATTACK = new Image("images/Bringer-of-Death_Attack_0.png", ENEMY_SIZE, ENEMY_SIZE, false, false);
+    public final static Image ENEMY_SPRITE_DEATH = new Image("images/Bringer-of-Death_Death_0.png", ENEMY_SIZE, ENEMY_SIZE, false, false);
+    private final Timeline attackAnimation;
+    private boolean movingRight;
+
+
+    public BasicEnemy(int x_pos, int y_pos) {
+        super(BasicEnemy.ENEMY_SPRITE_IDLE, x_pos, y_pos);
+        this.setTranslateX(x_pos);
+        this.setTranslateY(y_pos);
+
+        idleAnimation = createAnimation(8,"Bringer-of-Death_Idle_", ENEMY_SIZE, ENEMY_SIZE, Timeline.INDEFINITE, false);
+        moveAnimation = createAnimation(8,"Bringer-of-Death_Walk_", ENEMY_SIZE, ENEMY_SIZE, Timeline.INDEFINITE, false);
+        attackAnimation = createAnimation(10,"Bringer-of-Death_Attack_", ENEMY_SIZE, ENEMY_SIZE, Timeline.INDEFINITE, false);
+        deathAnimation = createAnimation(10,"Bringer-of-Death_Death_", ENEMY_SIZE, ENEMY_SIZE, Timeline.INDEFINITE, false);
+
+        // Start the animation
+        idleAnimation.play();
+//        idleAnimation.play();
+    }
+    public void move() {
+        if (!isAlive) return;
+        if (this.getTranslateX() >= this.orig_x + 100) movingRight = false;
+        if (this.getTranslateX() <= this.orig_x - 100) movingRight = true;
+        if (movingRight) {
+            setScaleX(-1);
+            this.setTranslateX(this.getTranslateX() + 0.25);
+        } else {
+            setScaleX(1);
+            this.setTranslateX(this.getTranslateX() - 0.25);
+        }
+        if (moveAnimation.getStatus() == Animation.Status.STOPPED) this.setImage(ENEMY_SPRITE_MOVE7);
+        idleAnimation.stop();
+        moveAnimation.play();
+    }
+    public void die() {
+        isAlive = false;
+        deathAnimation.play();
+    }
+}
--- src/game/StageMenu.java
+++ src/game/StageMenu.java
@@ -1,6 +1,5 @@
 package game;
 
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,35 +8,66 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class MainMenu {
+import java.util.HashMap;
+
+public class StageMenu {
+
+    public static final HashMap<String, Boolean> levels = new HashMap<>() {{
+        put("level1", true);
+        put("level2", false);
+        put("level3", false);
+    }};
+
     public void setStage(Stage primaryStage) {
 
         StackPane layout = new StackPane();
         layout.setPrefSize(1920, 1080);
-        VBox buttons = new VBox();
+        HBox buttons = new HBox();
         layout.setAlignment(Pos.CENTER);
         buttons.setAlignment(Pos.CENTER);
         buttons.setSpacing(20);
 
         Scene scene = new Scene(layout, 1920 , 1080);
-        Credits credits = new Credits(layout);
 
         Image bg = new Image("images/colored_land.png");
         ImageView image2 = new ImageView("images/play button.png");
         ImageView image3 = new ImageView("images/options_button.png");
         ImageView image4 = new ImageView("images/quit_button.png");
+
         Button button = new Button("", image2);
         Button button2 = new Button("", image3);
         Button button3 = new Button("", image4);
+        Button button4 = new Button("");
+
         button.setStyle("-fx-background-color: transparent;");
         button2.setStyle("-fx-background-color: transparent;");
         button3.setStyle("-fx-background-color: transparent;");
+        button4.setStyle("-fx-background-color: transparent;");
+
+        button4.setViewOrder(1);
+
         button.setOnAction(e -> {
-            StageMenu levelMenu = new StageMenu();
-            levelMenu.setStage(primaryStage);
+            if (levels.get("level1")) {
+                GameStage theStage = new GameStage(primaryStage, scene, LevelData.LEVEL1);
+                theStage.setStage(primaryStage);
+            }
         });
-        button2.setOnAction(e -> credits.openCredits(layout));
-        button3.setOnAction(e -> Platform.exit());
+        button2.setOnAction(e -> {
+            if (levels.get("level2")) {
+                GameStage theStage = new GameStage(primaryStage, scene, LevelData.LEVEL2);
+                theStage.setStage(primaryStage);
+            }
+        });
+        button3.setOnAction(e -> {
+            if (levels.get("level3")) {
+                GameStage theStage = new GameStage(primaryStage, scene, LevelData.LEVEL3);
+                theStage.setStage(primaryStage);
+            }
+        });
+        button4.setOnAction(e -> {
+            GameStage theStage = new GameStage(primaryStage, scene, LevelData.BONUS_LEVEL);
+            theStage.setStage(primaryStage);
+        });
 
         BackgroundImage backgroundimage = new BackgroundImage(bg,
                 BackgroundRepeat.REPEAT,
@@ -48,12 +78,14 @@ public class MainMenu {
         layout.setBackground(background);
 
 
-        buttons.getChildren().addAll(button, button2, button3);
+        buttons.getChildren().addAll(button, button2, button3, button4);
+
         layout.getChildren().addAll(buttons);
 
         primaryStage.setScene(scene);
         primaryStage.setFullScreen(true);
         primaryStage.setResizable(false);
-        primaryStage.show();
+
     }
+
 }
